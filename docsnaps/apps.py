326c19641afe31b6a37bbbd7513f984753db0205
monotonee <monotonee@tuta.io>
2016-10-12 17:08:58 -0400

    Added path to AppConfig.
    Changing the "docsnaps" to a namespace path caused Django to raise
    ImproperlyConfigured exception.

--- docsnaps/apps.py
+++ docsnaps/apps.py
@@ -1,6 +1,9 @@
+import os.path
+
 from django.apps import AppConfig
 
 
 class DocsnapsConfig(AppConfig):
     name = 'docsnaps'
+    path = os.path.dirname(__file__)
     verbose_name = 'document snapshots'
